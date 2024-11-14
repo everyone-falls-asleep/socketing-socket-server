95d56b5624a7e63b0c46bf60bcd76507c97610f8
hjyoon <hjyoon@kakao.com>
2024-11-14 14:25:21 +0900

    refactor: Simplify WebSocket server and enhance routing structure

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-// MessageObject represents a basic chat message object
 type MessageObject struct {
 	Data  string `json:"data"`
 	From  string `json:"from"`
@@ -19,20 +18,15 @@ type MessageObject struct {
 }
 
 func main() {
+
 	// Map to store connected clients
 	clients := make(map[string]string)
 
-	// Initialize a new Fiber app
+	// Start a new Fiber application
 	app := fiber.New()
 
-	// HTTP route
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World 👋!")
-	})
-
-	// WebSocket route group
-	wsGroup := app.Group("/ws", func(c *fiber.Ctx) error {
-		// Check if it's a WebSocket upgrade request
+	// Middleware to handle WebSocket upgrade
+	app.Use(func(c *fiber.Ctx) error {
 		if websocket.IsWebSocketUpgrade(c) {
 			c.Locals("allowed", true)
 			return c.Next()
@@ -40,67 +34,45 @@ func main() {
 		return fiber.ErrUpgradeRequired
 	})
 
-	// Define WebSocket route within the group
-	wsGroup.Get("/:id", socketio.New(func(kws *socketio.Websocket) {
-		// Retrieve user ID from the URL
-		userId := kws.Params("id")
-
-		// Add the connection to the list of connected clients
-		clients[userId] = kws.UUID
-
-		// Store the user ID in the WebSocket session
-		kws.SetAttribute("user_id", userId)
-
-		// Broadcast a message to all connected users
-		kws.Broadcast([]byte(fmt.Sprintf("New user connected: %s with UUID: %s", userId, kws.UUID)), true, socketio.TextMessage)
-
-		// Send a welcome message to the current user
-		kws.Emit([]byte(fmt.Sprintf("Hello user: %s with UUID: %s", userId, kws.UUID)), socketio.TextMessage)
-	}))
-
-	// SocketIO event handling
-	socketio.On(socketio.EventConnect, func(ep *socketio.EventPayload) {
-		fmt.Printf("Connection event - User: %s\n", ep.Kws.GetStringAttribute("user_id"))
+	// Serve the HTML page at the root
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendFile("./index.html")
 	})
 
-	socketio.On("CUSTOM_EVENT", func(ep *socketio.EventPayload) {
-		fmt.Printf("Custom event - User: %s\n", ep.Kws.GetStringAttribute("user_id"))
+	// Socket.io event handlers
+	socketio.On(socketio.EventConnect, func(ep *socketio.EventPayload) {
+		fmt.Printf("Connected: %s\n", ep.Kws.GetStringAttribute("user_id"))
 	})
 
 	socketio.On(socketio.EventMessage, func(ep *socketio.EventPayload) {
-		message := MessageObject{}
+		fmt.Printf("Message from %s: %s\n", ep.Kws.GetStringAttribute("user_id"), string(ep.Data))
+
+		var message MessageObject
 		err := json.Unmarshal(ep.Data, &message)
 		if err != nil {
 			fmt.Println("Error unmarshaling message:", err)
 			return
 		}
 
-		fmt.Printf("Message event - From: %s, To: %s, Data: %s\n", message.From, message.To, message.Data)
-
-		// Emit a custom event if specified
-		if message.Event != "" {
-			ep.Kws.Fire(message.Event, []byte(message.Data))
-		}
-
-		// Emit the message directly to the specified user
-		err = ep.Kws.EmitTo(clients[message.To], ep.Data, socketio.TextMessage)
-		if err != nil {
-			fmt.Println("Error emitting to user:", err)
-		}
+		// Echo the message back to the client
+		ep.Kws.Emit(ep.Data, socketio.TextMessage)
 	})
 
 	socketio.On(socketio.EventDisconnect, func(ep *socketio.EventPayload) {
-		userId := ep.Kws.GetStringAttribute("user_id")
-		delete(clients, userId)
-		fmt.Printf("Disconnection event - User: %s\n", userId)
+		delete(clients, ep.Kws.GetStringAttribute("user_id"))
+		fmt.Printf("Disconnected: %s\n", ep.Kws.GetStringAttribute("user_id"))
 	})
 
-	socketio.On(socketio.EventClose, func(ep *socketio.EventPayload) {
-		userId := ep.Kws.GetStringAttribute("user_id")
-		delete(clients, userId)
-		fmt.Printf("Close event - User: %s\n", userId)
-	})
+	// WebSocket route
+	app.Get("/ws/:id", socketio.New(func(kws *socketio.Websocket) {
+		userId := kws.Params("id")
+		clients[userId] = kws.UUID
+		kws.SetAttribute("user_id", userId)
 
-	// Start the server
+		// Send a welcome message
+		welcomeMsg := fmt.Sprintf("Hello user: %s with UUID: %s", userId, kws.UUID)
+		kws.Emit([]byte(welcomeMsg), socketio.TextMessage)
+	}))
+
 	log.Fatal(app.Listen(":3000"))
 }
